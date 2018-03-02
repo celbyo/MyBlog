4865ca3873e7ed798005335fe379e6ccec3cd737
william.guo <809908337@qq.com>
2018-03-02 15:57:04 +0800

    build app

--- webpack/entry.js
+++ webpack/entry.js
@@ -3,11 +3,14 @@ import path from 'path';
 const jsPath = path.join(__dirname, '../javascripts');
 
 const entry = {
-    'admin-index': [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
-        path.join(jsPath, '/admin')],
+    'admin-index':
+        // [
+        // 'react-hot-loader/patch',
+        // 'webpack-dev-server/client?http://localhost:8080',
+        // 'webpack/hot/only-dev-server',
+        path.join(jsPath, '/admin')
+    // ]
+    ,
     'client-index': path.join(jsPath, '/client'),
 };
 
